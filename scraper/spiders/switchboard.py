42a6f67aca6b74e24124b5ad9b08ac70ee0ccc98
Mgs. M. Rizqi Fadhlurrahman <rizqirizqi23@gmail.com>
2021-11-03 21:25:29 +0700

    Handle some special cases

--- scraper/spiders/switchboard.py
+++ scraper/spiders/switchboard.py
@@ -9,16 +9,23 @@ class SwitchboardSpider(scrapy.Spider):
         name_to_search = response.request.url.split('/')[-1].replace('%20', ' ')
         print('Looking up:', name_to_search)
         species_name = response.css('body > main > div:nth-child(2) > table tr:nth-child(2) td::text').get()
-        if not species_name:
+        species_dict = { 'Scientific Name': name_to_search }
+        if species_name:
+            species_dict['Found Species Name'] = species_name
+            species_dict['Switchboard'] = response.request.url
+        else:
             self.logger.info('Species not found!')
-        species_dict = { 'Scientific Name': species_name or name_to_search, 'Switchboard': response.request.url }
         search_result = response.css('#linksWrapper > div')
-        if search_result:
+        if len(search_result) > 0:
             icraflinks = search_result[0].css('h3::text')[0].get()
             if icraflinks == 'ICRAF Databases':
                 tables = search_result[0].css('div > div')[0].css('table')
-                for index, table in enumerate(tables):
-                    species_dict[table.css('th::text').get()] = table.css('a::attr(href)').get()
+                for table in tables:
+                    links = table.css('a')
+                    for link in links:
+                        if species_name in link.css('::text').get():
+                            db_name = " ".join(table.css('th::text').get().strip().split())
+                            species_dict[db_name] = link.css('::attr(href)').get()
             else:
                 self.logger.info('ICRAF Database section not found!')
-        yield species_dict
+        return species_dict
